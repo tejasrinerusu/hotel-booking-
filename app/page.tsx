fd5e56d9d2b3dd7c07f9d6db5ba7919cd590ed4f
NERUSUTEJASRI <149463311+tejasrinerusu@users.noreply.github.com>
2025-02-11 10:59:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,23 @@
 import { getHotels } from "@/actions/getHotels";
 import HotelList from "@/components/hotel/HotelList";
 
-interface HomeProps {
-  searchParams: {
-    title?: string;
-    country?: string;
-    state?: string;
-    city?: string;
-  };
+
+interface HomeProps{
+  searchParams:{
+    title:string,
+    country:string,
+    state:string,
+    city:string
+  }
 }
 
-export default async function Home({ searchParams }: HomeProps) {
-  // Ensure all properties have a default value
-  const sanitizedParams = {
-    title: searchParams.title ?? "",
-    country: searchParams.country ?? "",
-    state: searchParams.state ?? "",
-    city: searchParams.city ?? "",
-  };
+export default async function Home ({searchParams}:HomeProps) {
+  const hotels= await getHotels(searchParams)
 
-  const hotels = await getHotels(sanitizedParams);
-
-  if (!hotels || hotels.length === 0) return <div>No hotels found...</div>;
-
+  if(!hotels) return <div>No hotels found....</div>
   return (
     <div>
-      <HotelList hotels={hotels} />
+      <HotelList hotels={hotels}/>
     </div>
   );
 }
