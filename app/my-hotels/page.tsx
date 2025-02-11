0cafae6d40a0a082d6ef653ebe24a409c38b69e5
NERUSUTEJASRI <149463311+tejasrinerusu@users.noreply.github.com>
2025-02-11 11:00:04 +0530

    Update page.tsx

--- app/my-hotels/page.tsx
+++ app/my-hotels/page.tsx
@@ -1,24 +1,14 @@
 import { getHotelsByUserId } from "@/actions/getHotelsByUserId";
 import HotelList from "@/components/hotel/HotelList";
 
-const MyHotels = async () => {
-    try {
-        const hotels = await getHotelsByUserId();
+const MyHotels = async() => {
+    const hotels= await getHotelsByUserId()
 
-        if (!hotels || hotels.length === 0) {
-            return <div>No hotels found!</div>;
-        }
-
-        return (
-            <div>
-                <h2 className="text-2xl font-semibold">Here are your properties</h2>
-                <HotelList hotels={hotels} />
-            </div>
-        );
-    } catch (error) {
-        console.error("Error fetching hotels:", error);
-        return <div>Failed to load hotels. Please try again later.</div>;
-    }
-};
-
+    if(!hotels) return <div>No hotels found!</div>
+    return ( <div>
+        <h2 className="text-2xl font-semibold">Here are your properties</h2>
+        <HotelList hotels={hotels} />
+    </div> );
+}
+ 
 export default MyHotels;
